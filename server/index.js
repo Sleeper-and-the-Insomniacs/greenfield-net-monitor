ba9e47129ff18de7250d9fb98e7cda94768bd91a
ClaireHappel <clairehap1@gmail.com>
2026-06-29 19:33:36 -0500

    update / added sessions for google authentication + working on debugging endpoint for login

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
 const passport = require('passport');
-const authRouter = require('./routes/auth');
+const router = require('./routes/auth');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join('client', 'dist')));
 app.use(express.json());
+app.use(session({ secret: 'thisIsVerySecretSchenanigans' }));
 app.use(passport.initialize());
-app.use('/oauth2', authRouter);
+
+app.use('/oauth2', router);
+
+mongoose.connect('mongodb://127.0.0.1:27017/greenfield')
+  .then(() => {
+    console.info('Successfully connected to DB');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to DB', err);
+  });
 
 app.listen(port, () => {
   console.info(`
